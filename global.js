6f139c1de6a890fbf0e0bcd196701ef0dec9bac1
shriyakonda <166396390+shriyakonda@users.noreply.github.com>
2026-04-18 23:12:59 -0700

    g

---
+++ global.js
@@ -0,0 +1,81 @@
+console.log("IT'S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/Portfolio/";
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/shriyakonda', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+
+  a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+}
+
+// Dark mode switcher
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `<label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+
+let select = document.querySelector('.color-scheme select');
+
+function setColorScheme(colorScheme) {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+  select.value = colorScheme;
+}
+
+if ('colorScheme' in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', function (event) {
+  localStorage.colorScheme = event.target.value;
+  setColorScheme(event.target.value);
+});
+
+// Better contact form
+let form = document.querySelector('form');
+form?.addEventListener('submit', function (event) {
+  event.preventDefault();
+  let data = new FormData(form);
+  let url = form.action + '?';
+  let params = [];
+  for (let [name, value] of data) {
+    params.push(name + '=' + encodeURIComponent(value));
+  }
+  url += params.join('&');
+  location.href = url;
+});
